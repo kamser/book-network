f034257bbc0eb635b14d7a727fd753817dc3c611
kamser <keylor.morataya@gmail.com>
2024-04-29 22:29:15 -0700

    Create the monorepo structure, added the initial application configuration, created the docker compose for db and mail and created the user class

---
+++ book-network/src/main/java/com/kamser/booknetwork/user/User.java
@@ -0,0 +1,76 @@
+package com.kamser.booknetwork.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+    private String lastname;
+    private LocalDate dateOfBirth;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private boolean accountLocked;
+    private boolean enabled;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
